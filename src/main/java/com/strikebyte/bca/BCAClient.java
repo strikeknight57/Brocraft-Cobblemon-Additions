80c8462720d06f976f4c761aaebffb90d12e8fda
strikeknight57 <logan.brindley@gmail.com>
2024-11-07 23:32:57 -0700

    Initial commit

---
+++ src/main/java/com/strikebyte/bca/BCAClient.java
@@ -0,0 +1,10 @@
+package com.strikebyte.bca;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BCAClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
